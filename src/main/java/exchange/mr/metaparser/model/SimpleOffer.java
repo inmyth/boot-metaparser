b7014deb1980655ec3cf1080bea5067b20e140f0
Martin <martin@hanatoya.jp>
2018-02-01 15:45:44 +0900

    v.1.0.1

---
+++ src/main/java/exchange/mr/metaparser/model/SimpleOffer.java
@@ -0,0 +1,16 @@
+package exchange.mr.metaparser.model;
+
+public class SimpleOffer {
+	
+	public SimpleAmount quantity, totalPrice;
+
+	public SimpleOffer(SimpleAmount quantity, SimpleAmount totalPrice) {
+		super();
+		this.quantity = quantity;
+		this.totalPrice = totalPrice;
+	}
+	
+	
+	
+
+}
